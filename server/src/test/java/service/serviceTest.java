c4e2613f7fed8c70b5785989d94b57059ea45347
Tarotar30127 <carterl8@byu.edu>
2025-02-28 23:36:45 -0700

    Finished tests for Join, create, list game

--- server/src/test/java/service/serviceTest.java
+++ server/src/test/java/service/serviceTest.java
@@ -2,8 +2,12 @@ package service;
 import dataaccess.*;
 import exception.ResponseException;
 import model.AuthData;
+import model.GameData;
 import model.UserData;
+import model.joinColorId;
 import org.junit.jupiter.api.*;
+
+import java.util.Collection;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
@@ -81,6 +85,87 @@ public class serviceTest {
         });
         Assertions.assertEquals("Error: unauthorized", exception.getMessage());
     }
-
+    @Test
+    @DisplayName("Create Game Pass")
+    public void createGamePass() throws ResponseException {
+        UserData newUser = new UserData("tako", "legend", "@hotemail");
+        AuthData authData = service.registerUser(newUser);
+        assertDoesNotThrow(() -> {service.createGame("newGame",authData.authToken());});
+    }
+    @Test
+    @DisplayName("Login Fail Unauthorized")
+    public void createGameUnauthorized() throws ResponseException {
+        UserData newUser = new UserData("tako", "legend", "@hotemail");
+        AuthData authData = service.registerUser(newUser);
+        ResponseException exception = Assertions.assertThrows(ResponseException.class, () -> {
+            service.createGame("newGame", null);
+        });
+        Assertions.assertEquals("Error: unauthorized", exception.getMessage());
+    }
+    @Test
+    @DisplayName("Login Fail Bad Request")
+    public void createGameBadRequest() throws ResponseException {
+        UserData newUser = new UserData("tako", "legend", "@hotemail");
+        AuthData authData = service.registerUser(newUser);
+        ResponseException exception = Assertions.assertThrows(ResponseException.class, () -> {
+            service.createGame("", authData.authToken());
+        });
+        Assertions.assertEquals("Error: bad request", exception.getMessage());
+    }
+    @Test
+    @DisplayName("Join Game Pass")
+    public void joinGamePass() throws ResponseException {
+        UserData newUser = new UserData("tako", "legend", "@hotemail");
+        AuthData authData = service.registerUser(newUser);
+        int gameID = service.createGame("newGame",authData.authToken());
+        joinColorId newGame = service.joinGame(gameID, "WHITE", authData.authToken());
+        Assertions.assertEquals(newGame, new joinColorId("WHITE", gameID));
+    }
+    @Test
+    @DisplayName("Join Game Fail Unauthorized")
+    public void joinGameUnauthorized() throws ResponseException {
+        UserData newUser = new UserData("tako", "legend", "@hotemail");
+        AuthData authData = service.registerUser(newUser);
+        int gameID = service.createGame("newGame",authData.authToken());
+        joinColorId newGame = service.joinGame(gameID, "WHITE", authData.authToken());
+        ResponseException exception = Assertions.assertThrows(ResponseException.class, () -> {
+            service.joinGame(gameID, "WHITE", null);
+        });
+        Assertions.assertEquals("Error: unauthorized", exception.getMessage());
+    }
+    @Test
+    @DisplayName("Join Game Fail Bad Request")
+    public void joinGameBadRequest() throws ResponseException {
+        UserData newUser = new UserData("tako", "legend", "@hotemail");
+        AuthData authData = service.registerUser(newUser);
+        int gameID = service.createGame("newGame",authData.authToken());
+        joinColorId newGame = service.joinGame(gameID, "WHITE", authData.authToken());
+        ResponseException exception = Assertions.assertThrows(ResponseException.class, () -> {
+            service.joinGame(gameID, null, authData.authToken());
+        });
+        Assertions.assertEquals("Error: bad request", exception.getMessage());
+    }
+    @Test
+    @DisplayName("Game List Pass")
+    public void getGameListPass() throws ResponseException {
+        UserData newUser = new UserData("tako", "legend", "@hotemail");
+        AuthData authData = service.registerUser(newUser);
+        service.createGame("newGame", authData.authToken());
+        service.createGame("new", authData.authToken());
+        service.createGame("n", authData.authToken());
+        assertDoesNotThrow(() -> {Collection<GameData> games = service.getGame(authData.authToken());});
+    }
+    @Test
+    @DisplayName("Game List Fail Unauthorized")
+    public void getGameListFailUnauthorized() throws ResponseException {
+        UserData newUser = new UserData("tako", "legend", "@hotemail");
+        AuthData authData = service.registerUser(newUser);
+        int gameID = service.createGame("newGame",authData.authToken());
+        joinColorId newGame = service.joinGame(gameID, "WHITE", authData.authToken());
+        ResponseException exception = Assertions.assertThrows(ResponseException.class, () -> {
+            service.getGame(null);
+        });
+        Assertions.assertEquals("Error: unauthorized", exception.getMessage());
+    }
 
 }
