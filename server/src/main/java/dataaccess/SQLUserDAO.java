72aa33ae1f2ef27191381e94e5a7775ccc59e193
Tarotar30127 <carterl8@byu.edu>
2025-03-06 10:44:22 -0700

    worked on my sql userDAO

--- server/src/main/java/dataaccess/SQLUserDAO.java
+++ server/src/main/java/dataaccess/SQLUserDAO.java
@@ -1,12 +1,66 @@
 package dataaccess;
 
+import com.google.gson.Gson;
 import exception.ResponseException;
 import model.UserData;
 
-public class SQLUserDAO implements UserDAO{
-    @Override
-    public void createUser(UserData userData) throws ResponseException {
+import java.sql.Connection;
+import java.sql.SQLException;
 
+public class SQLUserDAO implements UserDAO {
+    private final String[] statement = { """
+            CREATE TABLE IF NOT EXISTS  userdata (
+               `id` int NOT NULL primary key AUTO_INCREMENT,
+               `username` varchar(256) NOT NULL,
+               `password` varchar( 256 ) NOT NULL,
+               `email` varchar( 256 ) NOT NULL
+             )
+            """
+    };
+    public SQLUserDAO() throws ResponseException, DataAccessException {
+        try {
+            configureDatabase();
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private void configureDatabase() throws DataAccessException, ResponseException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var state : statement) {
+                try (var preparedStatement = conn.prepareStatement(state)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new ResponseException(500, String.format("Unable to configure database: %s", ex.getMessage()));
+        }
+    }
+
+    @Override
+    public void createUser(UserData userData) throws ResponseException, SQLException {
+        Connection conn = null;
+        try {
+            conn = DatabaseManager.getConnection();
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        var json = new Gson().toJson(userData);
+        try {
+            String sql = "INSERT INTO users (username, password, email) VALUES ('%s','%s','%s','%s')"
+                    .formatted(userData.username(), userData.password(), userData.email());
+            try (var stmt = conn.createStatement()) {
+                int rowsAffected = stmt.executeUpdate(sql);
+                if (rowsAffected > 0) {
+                    System.out.println("ROW INSERTED");
+                } else {
+                    System.out.println("ROW NOT INSERTED");
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        conn.close();
     }
 
     @Override
@@ -19,3 +73,4 @@ public class SQLUserDAO implements UserDAO{
 
     }
 }
+
--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -3,9 +3,11 @@ package dataaccess;
 import model.UserData;
 import exception.ResponseException;
 
+import java.sql.SQLException;
+
 public interface UserDAO {
 
-    void createUser(UserData userData) throws ResponseException;
+    void createUser(UserData userData) throws ResponseException, SQLException;
     UserData getUser(String userName) throws ResponseException;
     void clear() throws ResponseException;
 }
