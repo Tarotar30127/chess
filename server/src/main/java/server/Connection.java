b5f625e50f4c77318abade6e82c228a7f6fec467
Tarotar30127 <carterl8@byu.edu>
2025-04-07 02:12:35 -0600

    Finished setting up the connection and leaveGame on the server side

--- server/src/main/java/server/Connection.java
+++ server/src/main/java/server/Connection.java
@@ -4,14 +4,7 @@ import org.eclipse.jetty.websocket.api.Session;
 
 import java.io.IOException;
 
-public class Connection {
-    public final org.eclipse.jetty.websocket.api.Session session;
-    public final int gameId;
-
-    public Connection(Session session, int gameId) {
-        this.session = session;
-        this.gameId = gameId;
-    }
+public record Connection(Session session, int gameId) {
 
     public void send(String message) throws IOException {
         session.getRemote().sendString(message);
--- server/src/main/java/server/WebSocketHandler.java
+++ server/src/main/java/server/WebSocketHandler.java
@@ -5,13 +5,18 @@ import com.google.gson.Gson;
 import exception.ResponseException;
 import model.AuthData;
 import model.GameData;
+import org.apache.spark.scheduler.OutputCommitCoordinator;
 import org.eclipse.jetty.websocket.api.RemoteEndpoint;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import org.sparkproject.jetty.proxy.ConnectHandler;
 import service.Service;
 import websocket.commands.*;
+import websocket.messages.LoadGame;
+import websocket.messages.ServerMessage;
 
+import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -26,7 +31,7 @@ public class WebSocketHandler {
     }
 
     @OnWebSocketMessage
-    public void onMessage(Session session, String msg) throws ResponseException {
+    public void onMessage(Session session, String msg) throws ResponseException, IOException {
         System.out.printf("Received: %s\n", msg);
 
         Matcher matcher = COMMAND_TYPE_PATTERN.matcher(msg);
@@ -63,17 +68,39 @@ public class WebSocketHandler {
         }
     }
 
-    private void connect(Session session, Connect command) throws ResponseException {
+    private void connect(Session session, Connect command) throws ResponseException, IOException {
         String authToken = command.getAuthToken();
         AuthData auth = service.getAuthProfile(authToken);
         int gameId = command.getGameID();
         GameData gameData = service.getOneGame(gameId);
         if (ChessGame.TeamColor.WHITE == Connect.getTeamColor()){
-
+            if (gameData.whiteUserName() != auth.username()){
+                var message = String.format("%s is not a player in the Game", auth.username());
+                var notification = new ServerMessage(ServerMessage.ServerMessageType.ERROR, message);
+                error(session, notification);
+            }
+        }else if (ChessGame.TeamColor.BLACK == Connect.getTeamColor()){
+            if (gameData.blackUserName() != auth.username()){
+                var message = String.format("%s is not a player in the Game", auth.username());
+                var notification = new ServerMessage(ServerMessage.ServerMessageType.ERROR, message);
+                error(session, notification);
+            }
         }
-
-
+        connections.add(session, gameId);
+        String serverMessage = String.format("%s has joined the game as %s".formatted(auth.username(), command.getTeamColor()));
+        ServerMessage msg = new ServerMessage(ServerMessage.ServerMessageType.NOTIFICATION, serverMessage );
+        ConnectionHandler.broadcast(session, msg, gameId);
+        LoadGame load = new LoadGame(gameData.game());
+        ConnectionHandler.broadcast(session, load, gameId);
     }
+
+
+
+    private void error(Session session, ServerMessage error) throws IOException {
+        System.out.printf("Error: %s%n", new Gson().toJson(error));
+        session.getRemote().sendString(new Gson().toJson(error));
+    }
+
 
     private void joinObserver(Session session, JoinObserver command) {
     }
@@ -81,7 +108,13 @@ public class WebSocketHandler {
     private void resign(Session session, Resign command) {
     }
 
-    private void leaveGame(Session session, Leave command) {
+    private void leaveGame(Session session, Leave command) throws ResponseException, IOException {
+        String authToken = command.getAuthToken();
+        AuthData auth = service.getAuthProfile(authToken);
+        connections.remove(session);
+        String serverMessage = String.format("%s has left the game".formatted(auth.username()));
+        ServerMessage msg = new ServerMessage(ServerMessage.ServerMessageType.NOTIFICATION, serverMessage );
+        ConnectionHandler.broadcast(session, msg, command.getGameID());
     }
 
     private void makeMove(Session session, Make_Move command) {
