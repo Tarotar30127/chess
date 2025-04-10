405cdad8d3198c5d8d42818506f5c6be6fd560c6
Tarotar30127 <carterl8@byu.edu>
2025-04-09 19:22:23 -0600

    Forgot to commit

---
+++ server/src/main/java/server/ConnectionHandler.java
@@ -0,0 +1,47 @@
+package server;
+
+import com.google.gson.Gson;
+import org.eclipse.jetty.websocket.api.Session;
+import websocket.messages.Notifcation;
+import websocket.messages.ServerMessage;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConnectionHandler {
+    private static final ConcurrentHashMap<Session, Connection> connections = new ConcurrentHashMap<>();
+
+    public void add(Session session, int gameId,String serveMessage) throws IOException {
+        var connection = new Connection(session, gameId);
+        connections.put(session, connection);
+        Notifcation notifcation = new Notifcation(serveMessage);
+        broadcast(notifcation, gameId, session);
+    }
+
+    public void remove(Session session) {
+        connections.remove(session);
+    }
+
+    public static void broadcast(ServerMessage notification, int gameId, Session senderSession) throws IOException {
+        String jsonNotification = new Gson().toJson(notification);
+
+        for (var entry : connections.entrySet()) {
+            Session session = entry.getKey();
+            Connection conn = entry.getValue();
+
+            if (session.isOpen() && conn.gameId() == gameId) {
+                if (!session.equals(senderSession)) {
+                    session.getRemote().sendString(jsonNotification);
+                }
+            }
+        }
+    }
+
+    public static void direct(ServerMessage message, Session session) throws IOException {
+        if (session != null && session.isOpen()) {
+            String jsonMessage = new Gson().toJson(message);
+            session.getRemote().sendString(jsonMessage);
+        }
+    }
+}
